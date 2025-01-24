92b56ba97f72d85954dadfb165d0794810aab96f
Robert Garofalo <r.s.garofalo87@gmail.com>
2025-01-25 10:52:04 +1100

    responsiveness for hero section complete

--- app/components/BookingForm.jsx
+++ app/components/BookingForm.jsx
@@ -35,13 +35,13 @@ const BookingForm = () => {
 
     return (
         <section className="flex flex-row justify-center lg:justify-start text-white order-3 lg:order-2">
-            <form onSubmit={handleSubmitForm} className="flex flex-col gap-y-4 border-[1.5px] border-white py-10 px-5 rounded-xl w-[380px]">
+            <form onSubmit={handleSubmitForm} className="flex flex-col gap-y-4 border-[1.5px] border-white py-5 lg:py-10 px-5 rounded-xl w-[380px]">
                 <h3 className='text-[28px] mb-5'>Make an appointment</h3>
                 <input className='text-[12px] placeholder-grey border-[1px] rounded py-2 px-4' type="text" label="name" placeholder="Name" />
                 <input className='text-[12px] placeholder-white bg-transparent border-[1px] rounded py-2 px-4' type="email" label="email" placeholder="Email" />
                 <input className='text-[12px] placeholder-white bg-transparent border-[1px] rounded py-2 px-4' type="number" label="phone" placeholder="Phone" />
                 <div className="flex flex-row justify-between pb-10">
-                    <input className='text-[12px] text-center text-white bg-transparent border-[1px] rounded py-2 px-2 ' type="datetime-local" label="name" onChange={(e) => setCurrentDateTime(e.target.value)} value={currentDateTime} />
+                    <input className='text-[12px] text-center text-white bg-transparent border-[1px] rounded py-2 sm:px-2 ' type="datetime-local" label="name" onChange={(e) => setCurrentDateTime(e.target.value)} value={currentDateTime} />
                     <button className='text-[12px] text-black border-[1px] border-accent bg-accent rounded px-3 '>Submit request</button>
                 </div>
             </form>
--- app/components/sections/Hero.jsx
+++ app/components/sections/Hero.jsx
@@ -15,8 +15,8 @@ const Hero = () => {
                 {/* bottom row */}
                 <div className="flex flex-col lg:flex-row justify-center items-center lg:justify-between lg:items-end 
                 lg:translate-y-[-40px] xl:translate-y-[0px] order-2">
-                    <p className="text-white text-[18px] mt-5 lg:mt-0 px-24 lg:px-0 lg:w-[500px] order-2 lg:order-1"><span className="hidden lg:flex">Our mission is to provide exceptional grooming services in a relaxed and friendly environment.</span> Whether you're here for a classic haircut, a modern fade, or a clean shave, our skilled barbers are dedicated to ensuring you leave looking and feeling your best.</p>
-                    <h1 className="text-white text-[70px] lg:text-[58px] xl:text-[78px] text-center lg:text-right font-semibold lg:leading-[80px] lg:w-[900px] uppercase order-1 lg:order-2 ">Where tradition <br />meets modern style.</h1>
+                    <p className="text-white text-center lg:text-left sm:text-[18px] mt-5 lg:mt-0 md:px-24 lg:px-0 lg:w-[500px] order-2 lg:order-1"><span className="hidden lg:flex">Our mission is to provide exceptional grooming services in a relaxed and friendly environment.</span> Whether you're here for a classic haircut, a modern fade, or a clean shave, our skilled barbers are dedicated to ensuring you leave looking and feeling your best.</p>
+                    <h1 className="text-white text-[40px] sm:text-[50px] md:text-[60px] lg:text-[58px] xl:text-[78px] text-center lg:text-right font-semibold lg:leading-[80px] lg:w-[900px] uppercase order-1 lg:order-2 ">Where tradition <br />meets modern style.</h1>
                 </div>
             </div>
         </section>
