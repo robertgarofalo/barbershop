0308191530d6fdbc5e25d6bdc36c3ad8e8dd51d4
Robert Garofalo <r.s.garofalo87@gmail.com>
2025-01-30 11:16:45 +1100

    work and prices ui and functionality

--- app/components/sections/Work.jsx
+++ app/components/sections/Work.jsx
@@ -1,7 +1,10 @@
 'use client'
 import { useState, useRef } from 'react'
 import Image from 'next/image';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+
+import { BsDash } from "react-icons/bs";
+
 
 // import required modules
 import { Pagination } from 'swiper/modules';
@@ -24,16 +27,31 @@ const images = [
     { name: 'barber image 10', path: '/assets/barber/10.jpg' },
 ]
 
+const testimonials = [
+    { name: 'James R.', text: "Best haircut I’ve ever had! The attention to detail was amazing, and the atmosphere was top-notch. The barber took his time to make sure everything was perfect. Definitely my go-to spot from now on!" },
+    { name: 'Michael T.', text: "Friendly staff, great vibes, and an even better haircut! They really listen to what you want and make sure you leave looking sharp. The hot towel shave was a game-changer. Highly recommend this place!" },
+    { name: 'Daniel S', text: "Top-quality service with a relaxed, professional touch. The fade was flawless, and the beard trim was on point. You can tell they care about their craft. I won’t be going anywhere else!" },
+]
+
+const pricing = [
+    { name: 'Haircut', price: '$40' },
+    { name: 'Shave', price: '$35' },
+    { name: 'Haircut + Shave', price: '$70' },
+    { name: 'Beard Trim', price: '$15' },
+]
+
 const Work = () => {
     const swiper1Ref = useRef(null)
     const [isFirstSlide, setIsFirstSlide] = useState(true)
     const [isLastSlide, setIsLastSlide] = useState(false)
 
+    const swiper2 = useSwiper()
+
     return (
-        <div className="w-full h-screen bg-[#ebe0cb] flex flex-row pl-20 py-16">
+        <div className="w-full h-screen bg-[#ebe0cb] flex flex-col pl-20 py-16">
 
             {/* row 1 - WORK */}
-            <div className='flex flex-row w-full overflow-hidden '>
+            <div className='flex flex-row w-full overflow-hidden mb-20'>
                 <div className='flex flex-col justify-evenly w-[30%] h-[50%] '>
                     <h1 className='text-[62px] font-semibold uppercase leading-[70px]'>Discover our work</h1>
                     <p>Our team of experienced barbers is dedicated to their craft. We stay updated with the latest techniques and styles to ensure you get the best service possible.</p>
@@ -69,7 +87,41 @@ const Work = () => {
             </div>
 
             {/* row 2 - PRICES*/}
-            <div></div>
+            <div className='flex flex-row justify-center gap-20'>
+                {/* testimonials */}
+                <div className='flex flex-col justify-between bg-[#fba95b] w-[450px] h-[300px] rounded-[20px] p-10'>
+                    <Swiper
+                        slidesPerView={1}
+                        // spaceBetween={40}
+                        className='flex flex-col max-w-full h-full'
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[Pagination]}
+                    >
+                        {testimonials.map((testimonial, index) => (
+                            <SwiperSlide key={`${testimonial.name}-testimonial`}>
+                                <p className=''>"{testimonial.text}"</p>
+                                <p className=' text-right font-semibold italic'>{testimonial.name}</p>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+
+                </div>
+
+                <div className='w-[700px] flex flex-col px-20'>
+                    <h1 className='uppercase text-[42px] font-semibold'>Our Prices</h1>
+                    {pricing.map((item, index) => (
+                        <div key={`${item.name}-${index}`} className='flex flex-col my-2 gap-3'>
+                            <div className='flex flex-row justify-between w-full'>
+                                <p>{item.name}</p>
+                                <p>{item.price}</p>
+                            </div>
+                            <hr className='border-[#b4b4b4]' />
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
